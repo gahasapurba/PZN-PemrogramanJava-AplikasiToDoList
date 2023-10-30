b261de6d67b9770cad7764cbfd6e1caf90d8a608
Gahasa Purba <gahasa.purba@ist.id>
2023-10-30 10:54:25 +0700

    39 - 43

--- src/AplikasiToDoList.java
+++ src/AplikasiToDoList.java
@@ -4,7 +4,91 @@ public class AplikasiToDoList {
 
     public static void main(String[] args) {
 
+        testShowToDoList();
 
+    }
+
+    /**
+     * Menampilkan ToDoList
+     */
+    public static void showToDoList(){
+
+        for (var i = 0 ; i < model.length ; i++) {
+            var todo = model[i];
+            var no = i + 1;
+
+            if (todo != null) {
+                System.out.println(no + ". " + todo);
+            }
+        }
+
+    }
+
+    public static void testShowToDoList() {
+        model[0] = "Belajar Java Dasar";
+        model[1] = "Studi Kasus Java Dasar : Aplikasi ToDoList";
+
+        showToDoList();
+    }
+
+    /**
+     * Menambah ToDoList
+     */
+    public static void addToDoList(String todo){
+        // Cek Apakah Model Penuh?
+        var isFull =true;
+        for (int i = 0; i < model.length; i++) {
+            if (model[i] == null) {
+                // Model Masih Ada Yang Kosong
+                isFull = false;
+                break;
+            }
+        }
+
+        // Jika Penuh, Kita Resize Ukuran Array 2X Lipat
+        if (isFull) {
+            var temp = model;
+            model = new String[model.length * 2];
+
+            for (int i = 0; i < temp.length; i++) {
+                model[i] = temp[i];
+            }
+        }
+
+        // Tambahkan Ke Posisi Yang Data Array Nya Null
+        for (var i = 0; i < model.length; i++) {
+            if (model[i] == null) {
+                model[i] = todo;
+                break;
+            }
+        }
+    }
+
+    /**
+     * Menghapus ToDoList
+     */
+    public static void removeToDoList(){
+
+    }
+
+    /**
+     * Menampilkan View ToDoList
+     */
+    public static void viewShowToDoList(){
+
+    }
+
+    /**
+     * Menampilkan View Menambah ToDoList
+     */
+    public static void viewAddToDoList(){
+
+    }
+
+    /**
+     * Menampilkan View Menghapus ToDoList
+     */
+    public static void viewRemoveToDoList(){
 
     }
 
